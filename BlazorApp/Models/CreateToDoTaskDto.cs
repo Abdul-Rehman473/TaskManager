64e387eecf4cf04fdf5f2fabe6dfe7c2627b5cc7
Rana Abdul Rehman <ranaabdulrehman0109@gmail.com>
2025-07-22 21:48:13 +0500

    Correct NavLinks

--- BlazorApp/Models/CreateToDoTaskDto.cs
+++ BlazorApp/Models/CreateToDoTaskDto.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
 {
     public class CreateToDoTaskDto
     {
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime? CompletedAt { get; set; }
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,18 +1,3 @@
-//using BlazorApp;
-//using BlazorApp.Services;
-//using Microsoft.AspNetCore.Components.Web;
-//using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-//var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5262/") });
-//builder.Services.AddScoped<TaskService>();
-
-//await builder.Build().RunAsync();
-
-
 using BlazorApp;
 using BlazorApp.Services;
 using Microsoft.AspNetCore.Components.Web;
