308d8d6724d5ae5cafb4837eab856f4fe0ffe91e
Rana Abdul Rehman <ranaabdulrehman0109@gmail.com>
2025-07-24 19:23:10 +0500

    Remove Comments

--- TaskManager.Infrastructure/DependencyInjection.cs
+++ TaskManager.Infrastructure/DependencyInjection.cs
@@ -1,28 +1,4 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.DependencyInjection;
-//using TaskManager.Core.Interfaces;
-//using TaskManager.Infrastructure.Data;
-//using TaskManager.Infrastructure.Repositories;
-
-//namespace MyApp.Infrastructure
-//{
-//    public static class DependencyInjection
-//    {
-//        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
-//        {
-//            services.AddDbContext<AppDbContext>(options =>
-//            {
-//                options.UseSqlServer("Server=.\\SQLEXPRESS;Database=NewTaskManagerDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
-//            });
-
-//            services.AddScoped<IToDoTaskRepository, ToDoTaskRepository>();
-
-//            return services;
-//        }
-//    }
-//}
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TaskManager.Core.Interfaces;
